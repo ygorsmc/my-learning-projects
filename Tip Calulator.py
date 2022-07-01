4feebc4ee375631c86fbd0f140651bf64d563203
Ygor Carvalho <106289554+ygorsmc@users.noreply.github.com>
2022-06-30 22:06:17 -0300

    Add files via upload
    My second project.

---
+++ Tip Calulator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# CALCULATOR TO SPLIT THE BILL
+
+print("Welcome to the tip calculator. Let's split the bill fairly!")
+
+total_bill = float(input("What was the total bill?\n"))
+extra_tax = float(input("What percentage it will have to increase?"))/100
+number_of_people = int(input("How many people should pay the bill?"))
+
+splitted_bill = (total_bill + total_bill*extra_tax)/number_of_people
+
+print(f"Each person should pay: $ {round(splitted_bill, 2)}")
