9bb3d36c2921ce19bd44bb4e31af213383f0dace
Ygor Carvalho <106289554+ygorsmc@users.noreply.github.com>
2022-07-02 17:06:38 -0300

    Update Rock, paper and scissor.py

--- Rock, paper and scissor.py
+++ Rock, paper and scissor.py
@@ -37,50 +37,63 @@ scissors = '''
 
 hand_list = [rock, paper, scissors]
 computer_list = hand_list
-print("Player choice:\n")
-hand = int(input("Which will you choose? 0, 1 or 2? (0 = rock, 1 = paper, 3 = scissors)\n"))
 
+gaming = True
 
-print(hand_list[hand])
-
-print("Computer choice: \n")
-
-random.shuffle(computer_list) 
-
-print(computer_list[0])
-
-print("Result:\n")
-
-# Identifyng the player choice
-if hand == 0:
-    player_choice = 'rock'
+while gaming:
     
-elif hand == 1:
-    player_choice = 'paper'
-    
-else:
-    player_choice = 'scissors'
+    print("Player choice:\n")
+    hand = int(input("Which will you choose? 0, 1 or 2? (0 = rock, 1 = paper, 3 = scissors)\n"))
 
-hand_list = [rock, paper, scissors]
-# Identifyng the player choice    
-if computer_list[0] == hand_list[0]:
-    computer_choice = 'rock'
-    
-elif computer_list[0] == hand_list[1]:
-    computer_choice = 'paper'
-    
-else:
-    computer_choice = 'scissors'
 
-# Comparing the choices   
-if player_choice == 'rock' and computer_choice == 'scissors' or player_choice == 'paper' and computer_choice == 'rock' or player_choice == 'scissors' and computer_choice == 'paper':
-    print("YOU WIN")
+    print(hand_list[hand])
 
-elif player_choice == computer_choice:
-    print("IT'S A DRAW!")
+    print("Computer choice: \n")
+
+    random.shuffle(computer_list) 
+
+    print(computer_list[0])
+
+    print("Result:\n")
+
+    # Identifyng the player choice
+    if hand == 0:
+        player_choice = 'rock'
+
+    elif hand == 1:
+        player_choice = 'paper'
+
+    else:
+        player_choice = 'scissors'
+
+    hand_list = [rock, paper, scissors]
+    # Identifyng the player choice    
+    if computer_list[0] == hand_list[0]:
+        computer_choice = 'rock'
+
+    elif computer_list[0] == hand_list[1]:
+        computer_choice = 'paper'
+
+    else:
+        computer_choice = 'scissors'
+
+    # Comparing the choices   
+    if player_choice == 'rock' and computer_choice == 'scissors' or player_choice == 'paper' and computer_choice == 'rock' or player_choice == 'scissors' and computer_choice == 'paper':
+        print("YOU WIN")
+
+    elif player_choice == computer_choice:
+        print("IT'S A DRAW!")
+
+    else:
+        print("YOU LOSE")
+
+    play_again = input("Do you wanna play again? ('yes' or 'no')\n")
     
-else:
-    print("YOU LOSE")
+    if play_again == 'yes':
+        gaming = True
+    else:
+        gaming = False
+    
 
 
 
