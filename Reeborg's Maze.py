ee4cade1ff75d1eb7c73c271b96e98343304553a
Ygor Carvalho <106289554+ygorsmc@users.noreply.github.com>
2022-07-04 23:10:54 -0300

    Update Reeborg's Maze.py
    Now it's run correctly.

--- Reeborg's Maze.py
+++ Reeborg's Maze.py
@@ -1,11 +1,14 @@
+while front_is_clear():
+    move()
+turn_left()
+
 while not at_goal():
-    if front_is_clear():
+    if right_is_clear():
+        turn_left()
+        turn_left()
+        turn_left()
         move()
-    elif right_is_clear(): # turn three times to the left to finally turn to right.
-        turn_left()
-        turn_left()
-        turn_left()
-    elif not right_is_clear() and front_is_clear():
+    elif front_is_clear():
         move()
     else:
         turn_left()
