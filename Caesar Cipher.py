6113426be601bfbbc7e7bf4f9e043fb4b4f5a60c
Ygor Carvalho <106289554+ygorsmc@users.noreply.github.com>
2022-07-06 21:35:34 -0300

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -2,46 +2,30 @@
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-def encode():
+def caesar():
+    
+    print("WELCOME TO CAESAR CIPHER! HERE YOUR MESSAGE WILL BE SAFE.")
+
+    is_action_valid = False
+
+    while is_action_valid == False:
+    
+        action = input("Type 'encode' to encrypt e 'decode' to decrypt:\n").lower()
+        
+        if action == 'encode' or action =='decode':
+            is_action_valid = True
+        else:
+            print("Please type 'encode' or 'decode'\n")
+                  
     message = input("Type your message:\n").lower()
     shift_number = int(input("Type the shift number:\n"))
     new_message = ''
     
-    for letter in message:
-        if alphabet.find(letter) + shift_number <= 25:
-            new_message += alphabet[alphabet.find(letter) + shift_number]
-        else:
-            new_message += alphabet[(alphabet.find(letter) + shift_number) - 25 - 1]
-    
-    print(f'Encrypted message: {new_message}')
-    
-def decode():
-    message = input("Type your message:\n").lower
-    shift_number = int(input("Type the shift number:\n"))
-    new_message = ''
-    
-    for letter in message:
-        if alphabet.find(letter) - shift_number >= 0:
-            new_message += alphabet[alphabet.find(letter) - shift_number]
-        else:
-            new_message += alphabet[(alphabet.find(letter) - shift_number) + 25 + 1]
-            
-    print(f'Decrypted message: {new_message}')
-    
-    
-print("WELCOME TO CAESAR CIPHER! HERE YOUR MESSAGE WILL BE SAFE.")
-
-is_data_valid = False
-
-while is_data_valid == False:
-    
-    action = input("Type 'encode' to encrypt e 'decode' to decrypt:\n").lower()
-
     if action == 'encode':
-        is_data_valid = True
-        encode()
-    elif action =='decode':
-        is_data_valid = True
-        decode()
+        for letter in message:
+            new_message += alphabet[(alphabet.find(letter) + shift_number)%26]
+        print(f'Encrypted message: {new_message}')
     else:
-        print("Please type 'encode' or 'decode'\n")
+        for letter in message:
+                new_message += alphabet[(alphabet.find(letter) - shift_number)%26]
+        print(f'Encrypted message: {new_message}')
