8bef2ed500fa8491b97c7fb3e8ae5a2aa9e7cdc9
Ygor Carvalho <106289554+ygorsmc@users.noreply.github.com>
2022-07-06 21:59:08 -0300

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -5,9 +5,9 @@ characters = 'abcdefghijklmnopqrstuvwxyz .,:!?0123456789'
 def caesar():
     
     print("WELCOME TO CAESAR CIPHER! HERE YOUR MESSAGE WILL BE SAFE.")
-
+    
+     # What action to do
     is_action_valid = False
-
     while is_action_valid == False:
     
         action = input("Type 'encode' to encrypt e 'decode' to decrypt:\n").lower()
@@ -16,15 +16,29 @@ def caesar():
             is_action_valid = True
         else:
             print("Please type 'encode' or 'decode'\n")
-                  
+    
+    # Message to work
     message = input("Type your message:\n").lower()
-    shift_number = int(input("Type the shift number:\n"))
+    
+    # Shift number
+    is_number = False
+    while is_number == False:
+    
+        shift_number = input("Type the shift number:\n")
+        
+        if shift_number.isdigit():
+            shift_number = int(shift_number)
+            is_number = True
+        else:
+            print("Plase type only numbers.")
+            
     new_message = ''
     
     if action == 'encode':
         for letter in message:
             new_message += characters[(characters.find(letter) + shift_number)%len(characters)]
         print(f'Encrypted message: {new_message}')
+
     else:
         for letter in message:
                 new_message += characters[(characters.find(letter) - shift_number)%len(characters)]
