18cdf2f2d1b7b1eff5b93425e4f6857f04653c5d
Ygor Carvalho <106289554+ygorsmc@users.noreply.github.com>
2022-07-15 22:58:45 -0300

    Update 14 - Higher Lower.py

--- 14 - Higher Lower.py
+++ 14 - Higher Lower.py
@@ -2,26 +2,25 @@
 
 import random
 
-list_of_comparisons = [("Cadbury", 200000), ("Uganda", 368000), ("Charlotte's Web", 165000), ("Tai Chi", 301000), ("The Prestige", 301000), ("2001 A Space Odyssey", 201000)]
-                                
-max_score = len(list_of_comparisons)
-
-random.shuffle(list_of_comparisons)
+def make_a_list():
+    list_of_entry = [("Cadbury", 200000), ("Uganda", 368000), ("Charlotte's Web", 165000), ("Tai Chi", 301000), ("The Prestige", 301000), ("2001 A Space Odyssey", 201000)]
+    random.shuffle(list_of_entry)
+    return list_of_entry
 
 def play_the_game():
     
+    list_of_comparisons = make_a_list()
+    max_score = len(make_a_list())-1
     score = 0
-    print("Welcome and enjoy the game!")
+    
+    print("Welcome and enjoy the game!\n")
+    
     is_gaming = True    
     while is_gaming:
 
-        print(f'''
-        {list_of_comparisons[0][0]} 
-         has 
-        {list_of_comparisons[0][1]} 
-        average monthly searches\n''')
+        print(f"{list_of_comparisons[0][0]}\nhas\n{list_of_comparisons[0][1]}\naverage monthly searches\n")
         print("VS\n")
-        print(list_of_comparisons[1][0])
+        print(f"{list_of_comparisons[1][0]}\n")
 
         while True:
 
@@ -31,7 +30,7 @@ def play_the_game():
                 if list_of_comparisons[1][1] >= list_of_comparisons[0][1]:
                     score += 1
                     print(f"You are right!")
-                    print(f"Your score is: {score}")
+                    print(f"Your score is: {score}\n")
                     break
                 else:
                     print("YOU LOSE...")
@@ -47,17 +46,17 @@ def play_the_game():
                     break
                 else:
                     print("YOU LOSE...")
-                    print(f"Your final score is {score}")
+                    print(f"Your final score is {score}\n")
                     is_gaming = False
                     break
             else:
                 print("Please, type only 'higher' or 'lower'!")
-
+            
         if score == max_score:
             print("YOU ARE AWESOME! THERE IS NOTHING YOU DO NOT KNOW!")
             break
-
-        list_of_comparisons.pop(0)
+        else:
+            list_of_comparisons.pop(0)
 
 play_the_game()
 
